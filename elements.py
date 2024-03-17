d225615b865329b79917186de37bfeadb0ed6629
sergkrumas <cjkrumas@gmail.com>
2024-03-17 14:55:48 +0300

    bug fixed, see CHANGELOG.md

--- elements.py
+++ elements.py
@@ -1932,7 +1932,7 @@ class ElementsMixin(ElementsTransformMixin):
 
     def elementsTextFieldInputEvent(self, event):
         ae = self.active_element
-        if ae.type != ToolID.text:
+        if ae is None or ae.type != ToolID.text:
             return
 
         if event.modifiers() == Qt.ControlModifier and check_scancode_for(event, "V"):
@@ -1993,7 +1993,7 @@ class ElementsMixin(ElementsTransformMixin):
         is_event = self.active_element is not None and self.active_element.type == ToolID.text
         is_event = is_event and event.key() != Qt.Key_Escape
         is_event = is_event and event.key() not in [Qt.Key_Delete, Qt.Key_Insert, Qt.Key_Home, Qt.Key_End, Qt.Key_PageDown, Qt.Key_PageUp]
-        is_event = is_event and bool(event.text()) or (event.key() in [Qt.Key_Left, Qt.Key_Right])
+        is_event = is_event and (bool(event.text()) or (event.key() in [Qt.Key_Left, Qt.Key_Right]))
         is_event = is_event and (not event.modifiers() or (event.modifiers() == Qt.ControlModifier and check_scancode_for(event, "V")))
         return is_event
 

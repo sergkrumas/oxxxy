c379c90d532c5e43e3e7efcd00f7e1f1ff610680
sergkrumas <cjkrumas@gmail.com>
2024-03-22 04:32:28 +0300

    pylint

--- run_pylint.bat
+++ run_pylint.bat
@@ -2,4 +2,6 @@
 del pylint_out.txt
 
 pylint oxxxy.py  --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
+pylint oxxxy_aux_ui.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
+pylint oxxxy_editor_ui.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
 pylint _utils.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt

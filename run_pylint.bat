6d9e9870a9e9c0b42078c7c49c4a41924d29deae
sergkrumas <cjkrumas@gmail.com>
2024-03-28 02:50:51 +0300

    pylint bat updated

--- run_pylint.bat
+++ run_pylint.bat
@@ -11,3 +11,4 @@ pylint elements_tools2024.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
 pylint editor_autotest.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
 pylint _utils.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
 pylint _sliders.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt
+pylint  image_viewer_lite.py --disable=E0611,C0115,C0103,C0116 >> pylint_out.txt

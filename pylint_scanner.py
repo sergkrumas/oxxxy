9dd9ec75ffd61df29ae32d6c3786c39c84c8cb47
sergkrumas <cjkrumas@gmail.com>
2024-03-28 06:12:34 +0300

    refactoring

--- pylint_scanner.py
+++ pylint_scanner.py
@@ -14,15 +14,16 @@ def scan():
                 filepath = os.path.join(cur_dir, filename)
                 filepaths.append(filepath)
 
-    filepath = os.path.join(root, 'pylint_out.txt')
-    if os.path.exists(filepath):
-        os.remove(filepath)
-    f = open(filepath, "w", encoding='utf8')
+    report_filepath = os.path.join(root, 'pylint_out.txt')
+    if os.path.exists(report_filepath):
+        os.remove(report_filepath)
+    f = open(report_filepath, "w", encoding='utf8')
     count = len(filepaths)
     for n, filepath in enumerate(filepaths, start=1):
         status = f'[{n}/{count}]'
-        print(f'scanning file {status} {filepath}...')
+        print(f'{status} Сканирую файл {filepath}...')
         subprocess.call(["pylint", filepath, "--disable=E0611,C0115,C0103,C0116"], stdout=f)
+    print(f'\nОтчёт сформирован в файле {report_filepath}')
 
 if __name__ == '__main__':
     scan()

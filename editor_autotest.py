b9010f890c41a61e66cf3f594a609d122027087c
sergkrumas <cjkrumas@gmail.com>
2024-03-28 03:36:03 +0300

    pylint cleaning

--- editor_autotest.py
+++ editor_autotest.py
@@ -1,9 +1,7 @@
 
 
 from PyQt5.QtWidgets import (QApplication,)
-from PyQt5.QtCore import (pyqtSignal, QPoint, QPointF, pyqtSlot, QRect, QEvent,
-    QTimer, Qt, QSize, QSizeF, QRectF, QThread, QAbstractNativeEventFilter,
-    QAbstractEventDispatcher, QFile, QDataStream, QIODevice)
+from PyQt5.QtCore import (QPointF, QEvent, Qt)
 from PyQt5.QtGui import (QPainterPath, QColor, QKeyEvent, QMouseEvent, QBrush, QPixmap,
     QPaintEvent, QPainter, QWindow, QPolygon, QImage, QTransform, QPen, QLinearGradient,
     QIcon, QFont, QCursor, QPolygonF, QVector2D, QFontDatabase)
